fd23d1268125c8597c2d4d537f76bc99010c22a5
Mariana CF <mari.marianacaliman@gmail.com>
2023-03-05 23:42:01 -0300

    remove unused code and changed background color

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,7 +14,17 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-     
+      <main className={styles.main}>
+
+
+        
+
+        <div className={styles.navigation}>
+          <a href='#'>Cat</a>
+
+          
+        </div>
+      </main>
     </>
   )
 }
